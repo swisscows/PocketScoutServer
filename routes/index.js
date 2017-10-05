4927ddcf6feb9542d95e79e96513bd9008edba70
Ben <swisscows2@gmail.com>
2017-10-05 13:24:09 -0500

    Installed Modules and Added TODOs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var restrictedChars = "\/'+=-_|[](){},.?!@#$%^&*:;\"";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/*TODO: addUser */
+
+/*TODO: removeUser */
+
+/*TODO: requestToken */
+
+/*TODO: returnToken */
+
+/*TODO: getAllForGroup */
+
+/*TODO: addGeo */
+
+/*TODO: removeGeo */
+
+/*TODO: addCampingListItem*/
+
+/*TODO: removeCampingListItem */
+
+/*TODO: updateCampingListItem */
+
+/*TODO: newMessage */
+
+/*TODO: removeMessage */
+
+/*TODO: getAllMessages */
+
+/*TODO: getNewMessages */
 
 module.exports = router;
